85d6118da824aa5386662c92988971fcb54d9533
fredyhg <fredyultima2@gmail.com>
2024-08-27 15:39:53 -0300

    feat: create Client Service interface and move some methods to helper methods

--- src/main/java/com/squad7/desafiolocadorasolutis/service/ClientService.java
+++ src/main/java/com/squad7/desafiolocadorasolutis/service/ClientService.java
@@ -1,24 +1,7 @@
 package com.squad7.desafiolocadorasolutis.service;
 
-import com.squad7.desafiolocadorasolutis.exception.ClientAlreadyExistsException;
-import com.squad7.desafiolocadorasolutis.model.Client;
-import com.squad7.desafiolocadorasolutis.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.squad7.desafiolocadorasolutis.controller.request.ClientPostRequest;
 
-@Service
-public class ClientService {
-
-    @Autowired
-    private ClientRepository clientRepository;
-
-    public void registerClient(Client client) {
-        if (clientRepository.existsByCpf(client.getCpf())) {
-            throw new ClientAlreadyExistsException("Client with CPF " + client.getCpf() + " already exists.");
-        }
-        if (clientRepository.existsByEmail(client.getEmail())) {
-            throw new ClientAlreadyExistsException("Client with email " + client.getEmail() + " already exists.");
-        }
-        clientRepository.save(client);
-    }
+public interface ClientService {
+    public void registerClient(ClientPostRequest clientPostRequest);
 }
