b1be2e679aefa81ada6b9b5945bd42dbf5c1b2c5
renatof4rias <renatof4rias@gmail.com>
2024-08-29 08:24:17 -0300

    feat: acceptTermsAndServices ClientServiceImpl

--- src/main/java/com/squad7/desafiolocadorasolutis/service/impl/ClientServiceImpl.java
+++ src/main/java/com/squad7/desafiolocadorasolutis/service/impl/ClientServiceImpl.java
@@ -5,12 +5,15 @@ import com.squad7.desafiolocadorasolutis.exception.ClientAlreadyExistsException;
 import com.squad7.desafiolocadorasolutis.mappers.ClientMapper;
 import com.squad7.desafiolocadorasolutis.model.Client;
 import com.squad7.desafiolocadorasolutis.repository.ClientRepository;
+import com.squad7.desafiolocadorasolutis.service.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class ClientServiceImpl {
+public class ClientServiceImpl implements ClientService {
 
 
     private final ClientRepository clientRepository;
@@ -37,4 +40,30 @@ public class ClientServiceImpl {
             throw new ClientAlreadyExistsException("Client with CPF " + cpf + " already exists.");
         }
     }
+
+    @Override
+    public boolean existsByCpf(String cpf){
+        return clientRepository.existsByCpf(cpf);
+    }
+
+    @Override
+    public void acceptTermsAndServices(String cpf) {
+        Optional<Client> client = clientRepository.getByCpf(cpf);
+        if (client.isPresent()){
+            Client clientToSave = client.get();
+            clientToSave.setBlocked(false);
+            clientRepository.save(clientToSave);
+        }
+    }
+
+    @Override
+    public Client getByCpf(String cpf) {
+        return clientRepository.getByCpf(cpf).get();
+    }
+
+    @Override
+    public Client getByCpfAndBlockedFalse(String cpf) {
+        return clientRepository.getByCpfAndBlockedFalse(cpf).orElse(null);
+    }
+
 }
