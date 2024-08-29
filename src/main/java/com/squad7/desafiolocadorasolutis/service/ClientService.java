36e892f2ce854ef0da267f77ac310c59af9b2766
renatof4rias <renatof4rias@gmail.com>
2024-08-29 08:23:27 -0300

    feat: getByCpfAndBlockedFalse ClientService

--- src/main/java/com/squad7/desafiolocadorasolutis/service/ClientService.java
+++ src/main/java/com/squad7/desafiolocadorasolutis/service/ClientService.java
@@ -1,7 +1,18 @@
 package com.squad7.desafiolocadorasolutis.service;
 
 import com.squad7.desafiolocadorasolutis.controller.request.ClientPostRequest;
+import com.squad7.desafiolocadorasolutis.model.Client;
 
 public interface ClientService {
     public void registerClient(ClientPostRequest clientPostRequest);
+
+    boolean existsByCpf(String cpf);
+
+    void acceptTermsAndServices(String cpf);
+
+    Client getByCpf(String cpf);
+
+    Client getByCpfAndBlockedFalse(String cpf);
+
+
 }
