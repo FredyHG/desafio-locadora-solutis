5f941418ce0eaf0d67fea9178da6b6133cc0d659
fredyhg <fredyultima2@gmail.com>
2024-08-31 15:55:47 -0300

    feat: create method to recover driver ignore any status

--- src/main/java/com/squad7/desafiolocadorasolutis/repository/CarRentalRepository.java
+++ src/main/java/com/squad7/desafiolocadorasolutis/repository/CarRentalRepository.java
@@ -1,9 +1,13 @@
 package com.squad7.desafiolocadorasolutis.repository;
 
+import com.squad7.desafiolocadorasolutis.enums.CarRentalStatus;
 import com.squad7.desafiolocadorasolutis.model.CarRental;
+import com.squad7.desafiolocadorasolutis.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CarRentalRepository extends JpaRepository<CarRental, UUID> {
+    List<CarRental> findByDriverAndRentalStatusNotIn(Driver driver, List<CarRentalStatus> carRentalStatuses);
 }
