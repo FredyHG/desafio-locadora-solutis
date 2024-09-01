046e734183eab172e4491e93992bc569b3313fd6
Pietra Almeida <costapietra@gmail.com>
2024-09-01 17:13:32 -0300

    feat: add Swagger documentation for TermsController

--- src/main/java/com/squad7/desafiolocadorasolutis/controller/TermsController.java
+++ src/main/java/com/squad7/desafiolocadorasolutis/controller/TermsController.java
@@ -1,8 +1,17 @@
 package com.squad7.desafiolocadorasolutis.controller;
 
 import com.squad7.desafiolocadorasolutis.controller.response.ResponseMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 
 public interface TermsController {
+
+    @Operation(summary = "Accept terms of service", description = "Accepts the terms of service for a user identified by CPF.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Terms accepted successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid CPF provided")
+    })
     ResponseEntity<ResponseMessage> acceptTerms(String cpf);
 }
