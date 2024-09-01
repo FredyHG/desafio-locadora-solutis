a5e6a081c59fc3d215092d63ecd57664c148d2a1
Syllas <syllasbraga2@gmail.com>
2024-09-01 16:40:28 -0300

    feat: get all employes

--- src/main/java/com/squad7/desafiolocadorasolutis/controller/response/EmployeeResponse.java
+++ src/main/java/com/squad7/desafiolocadorasolutis/controller/response/EmployeeResponse.java
@@ -1,8 +1,6 @@
 package com.squad7.desafiolocadorasolutis.controller.response;
 
-import com.squad7.desafiolocadorasolutis.enums.AccountEmailStatusEnum;
 import com.squad7.desafiolocadorasolutis.enums.SexEnum;
-import com.squad7.desafiolocadorasolutis.model.Terms;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,15 +11,13 @@ import java.util.UUID;
 @Getter
 @Setter
 @AllArgsConstructor
-public class DriverResponse {
+public class EmployeeResponse {
 
     private UUID id;
     private String name;
     private String cpf;
     private String email;
-    private String cnhNumber;
-    private LocalDate birthDate;
     private SexEnum sexEnum;
-    private Terms accountTerms;
-    private AccountEmailStatusEnum accountEmailStatusEnum;
+    private LocalDate birthDate;
+    private String registration;
 }
